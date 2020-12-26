ddb22381ace94b14d4322154d155649911a2ec04
Atul Kumar <atul.kumar@mavenir.com>
2020-12-26 12:00:00 +0530

    Added terraform IAC

---
+++ outputs.tf
@@ -0,0 +1,21 @@
+output "vpc_provisiong"{
+
+    value = aws_vpc.my-vpc
+}
+output "subnet_provisiong"{
+
+    value = aws_subnet.my-subnet-public-1
+}
+
+output "route_provisiong"{
+
+    value = aws_route_table.my-public-rt
+}
+output "ig_provisiong"{
+
+    value = aws_internet_gateway.my-igw
+}
+output "ec2_provisioning"{
+
+    value = aws_instance.my-ec2-instance
+}
